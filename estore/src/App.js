270936827461fcd9c09179acb02f9bb409a156c9
Priyanka <priyanka@Priyankas-MacBook-Air.local>
2025-10-23 01:05:31 +0530

    create-react-app

---
+++ estore/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    
+  <div> 
+  <h2>Name :Name1</h2>
+  <h2> Age: 32 </h2>
+</div>
+  
+
+    </div>
+  );
+}
+
+export default App;
