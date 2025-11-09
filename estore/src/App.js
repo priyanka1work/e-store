ea179aca693966c7c55eaa053455cb8fc574046d
Priyanka <priyanka@Priyankas-MacBook-Air.local>
2025-11-09 16:18:11 +0530

    begins here

--- estore/src/App.js
+++ estore/src/App.js
@@ -1,19 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    
-  <div> 
-  <h2>Name :Name1</h2>
-  <h2> Age: 32 </h2>
-</div>
-  
-
-    </div>
-  );
+  return <div className="App"> </div>;
 }
 
 export default App;
